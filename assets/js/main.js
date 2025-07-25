5f6fc1d70cd10b783c6721792a8127940b6ed355
Sanjeet Sinha <sanjeetkumarsinha1@gmail.com>
2025-07-25 08:18:48 +0530

    Contact form configured

--- assets/js/main.js
+++ assets/js/main.js
@@ -220,3 +220,26 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+  function showThankYou(event) {
+    event.preventDefault(); // stop default submission
+
+    const form = event.target;
+    const formData = new FormData(form);
+
+    fetch(form.action, {
+      method: form.method,
+      body: formData,
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        form.reset();
+        form.style.display = 'none';
+        document.getElementById('thank-you-msg').style.display = 'block';
+      } else {
+        alert("Something went wrong!");
+      }
+    });
+  }
